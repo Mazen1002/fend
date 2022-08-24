d37782849dde141e2546aeb3ea5fe9fdf59fb775
Mazen1002 <112000457+Mazen1002@users.noreply.github.com>
2022-08-24 18:34:39 +0200

    Update app.js

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -1,27 +1,6 @@
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
 
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
-*/
 
-/**
- * Define Global Variables
- * 
-*/
+
 // beginning gobal variables
 const navBar = document.querySelector('.navbar__menu')
 const navList = document.querySelector('#navbar__list');
